1313f282a999f69cbee68de1a7147077b8c3b487
zblxst <varin.thomas.2002@gmail.com>
2024-11-04 16:49:48 +0100

    Added some instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -37,6 +37,126 @@ impl CPU {
     }
 
 
+    // Loads operand into Accumulator
+    fn lda(&mut self, operand: u8) {
+        self.reg_a = operand;
+        self.update_n_flag(self.reg_a);
+        self.update_z_flag(self.reg_a);
+    }
+
+    // Loads operand into X register
+    fn ldx(&mut self, operand: u8) {
+        self.reg_x = operand;
+        self.update_n_flag(self.reg_x);
+        self.update_z_flag(self.reg_x);
+    }
+
+    // Loads operand into Y register
+    fn ldy(&mut self, operand: u8) {
+        self.reg_y = operand;
+        self.update_n_flag(self.reg_y);
+        self.update_z_flag(self.reg_y);
+    }
+
+    // Transfer Accumulator to X register
+    fn tax(&mut self) {
+        self.reg_x = self.reg_a;
+        self.update_n_flag(self.reg_x);
+        self.update_z_flag(self.reg_x);
+    }
+
+    // Transfer X register to Accumulator
+    fn txa(&mut self) {
+        self.reg_a = self.reg_x;
+        self.update_n_flag(self.reg_a);
+        self.update_z_flag(self.reg_a);
+    }
+
+    // Transfer SP register to X register
+    fn tsx(&mut self) {
+        self.reg_x = self.reg_sp;
+        self.update_n_flag(self.reg_x);
+        self.update_z_flag(self.reg_x);
+    }
+
+    // Transfer X register to SP register
+    fn txs(&mut self) {
+        self.reg_sp = self.reg_x;
+    }
+
+    // Transfer Accumulator to Y register
+    fn tay(&mut self) {
+        self.reg_y = self.reg_a;
+        self.update_n_flag(self.reg_y);
+        self.update_z_flag(self.reg_y);
+    }
+
+    // Transfer Y register to Accumulator
+    fn tya(&mut self) {
+        self.reg_a = self.reg_y;
+        self.update_n_flag(self.reg_a);
+        self.update_z_flag(self.reg_a);
+    }
+
+    // Logical and between a value and Accumulator
+    fn and(&mut self, operand: u8) {
+        self.reg_a &= operand;
+        self.update_n_flag(self.reg_a);
+        self.update_z_flag(self.reg_a);
+    }
+
+    // Logical xor between a value and Accumulator
+    fn eor(&mut self, operand: u8) {
+        self.reg_a ^= operand;
+        self.update_n_flag(self.reg_a);
+        self.update_z_flag(self.reg_a);
+    }
+
+    // Logical or between a value and Accumulator
+    fn ora(&mut self, operand: u8) {
+        self.reg_a |= operand;
+        self.update_n_flag(self.reg_a);
+        self.update_z_flag(self.reg_a);
+    }
+
+    // Increment X register
+    fn inx(&mut self) {
+        self.reg_x += 1;
+        self.update_n_flag(self.reg_x);
+        self.update_z_flag(self.reg_x);
+    }
+
+    // Increment Y register
+    fn iny(&mut self) {
+        self.reg_y += 1;
+        self.update_n_flag(self.reg_y);
+        self.update_z_flag(self.reg_y);
+    }
+
+    // Set carry flag
+    fn sec(&mut self) {
+        self.status |= 0b0000_0001;
+    }
+
+    // Set decimal flag
+    fn sed(&mut self) {
+        self.status |= 0b0000_1000;
+    }
+
+    // Set interruption disable flag
+    fn sei(&mut self) {
+        self.status |= 0b0000_0100;
+    }
+
+    // Jump to a spectified address
+    fn jmp(&mut self, operand: u16) {
+        self.reg_pc = operand;
+    }
+
+
+
+
+
     pub fn interpret(&mut self, program: &Vec<u8>) {
         self.reg_pc = 0;
         loop {
@@ -45,58 +165,79 @@ impl CPU {
         
             println!("{:?}", opcode);
             match opcode {
-                // LDA 
-                // Loads operand into accumulator
                 0xa9 => {
                     let operand: u8 = program[self.reg_pc as usize];
                     self.reg_pc += 1;
-                    self.reg_a = operand;
-
-                    self.update_n_flag(self.reg_a);
-                    self.update_z_flag(self.reg_a);
+                    self.lda(operand);
                 }
 
-                // AND
-                // logical and between a Memory adress and Accumulator
+                0xa2 => {
+                    let operand: u8 = program[self.reg_pc as usize];
+                    self.reg_pc += 1;
+                    self.ldx(operand);
+                }
+
+                0xa0 => {
+                    let operand: u8 = program[self.reg_pc as usize];
+                    self.reg_pc += 1;
+                    self.ldy(operand);
+                }
+
                 0x29 => {
                     let operand: u8 = program[self.reg_pc as usize];
                     self.reg_pc += 1;
-                    self.reg_a &= operand;
-
-                    self.update_n_flag(self.reg_a);
-                    self.update_z_flag(self.reg_a);
+                    self.and(operand);
                 }
 
-                // TAX
-                // Transfer Accumulator to X register
-                0xAA => {
+                0x49 => {
+                    let operand: u8 = program[self.reg_pc as usize];
                     self.reg_pc += 1;
-                    self.reg_x = self.reg_a;
-
-                    self.update_n_flag(self.reg_x);
-                    self.update_z_flag(self.reg_x);
+                    self.eor(operand);
                 }
 
-                // INX
-                // Transfer Accumulator to X register
-                0xE8 => {
+                0x09 => {
+                    let operand: u8 = program[self.reg_pc as usize];
                     self.reg_pc += 1;
-                    self.reg_x +=1;
-
-                    self.update_n_flag(self.reg_x);
-                    self.update_z_flag(self.reg_x);
+                    self.ora(operand);
                 }
+
+                0xAA => self.tax(),
+                0x8A => self.txa(),
+
+                0xA8 => self.tay(),
+                0x98 => self.tya(),
+
+                0xBA => self.tsx(),
+                0x9A => self.txs(),
+
+                0xE8 => self.inx(),
+                0xC8 => self.iny(),
+
+                0x38 => self.sec(),
+
+                0xF8 => self.sed(),
+
+                0x78 => self.sei(),
+
+                // NOP
+                0xEA => (),
+
+
+                0x4C => {
+                    let low_operand: u16 = program[self.reg_pc as usize].into();
+                    self.reg_pc += 1;
+                    let high_operand: u16 = program[self.reg_pc as usize].into();
+                    self.reg_pc += 1;
+                    self.jmp(high_operand << 8 | low_operand);
+                }
+
+
+
 
                 // BRK
                 0x00 => {
                     self.status |= 0b0010_0000;
                     return
-                }
-                // Return 
-                // Just for the tests
-                0xff => {
-                    println!("{:?}", self);
-                    return;
                 }
                 _ => todo!("Opcode not implemented")
             }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ fn main() {
     println!("Hello, world!");
     let mut cpu : CPU = CPU::new();
 
-    let program : Vec<u8> = vec![0xa9, 0xc0, 0xff];
+    let program : Vec<u8> = vec![0xa9, 0xc0, 0x00];
     println!("{:?}", cpu);
     cpu.interpret(&program);
+    println!("{:?}", cpu);
+
 }
